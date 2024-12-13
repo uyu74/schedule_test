5c699fdffa4f1ef48ccd68a1b22fe053957ead9f
FU YONGQI <148615767+uyu74@users.noreply.github.com>
2024-12-13 22:47:45 +0800

    完整了table页面对于展示课表的选择

--- unpackage/dist/dev/mp-weixin/pages/schedule_table/schedule_table.js
+++ unpackage/dist/dev/mp-weixin/pages/schedule_table/schedule_table.js
@@ -3,22 +3,40 @@ const common_vendor = require("../../common/vendor.js");
 const _sfc_main = {
   data() {
     return {
-      schedules: this.getSchedulesFromStorage(),
+      schedules: [],
       // 获取存储中的课程数据
       scheduleOnShow: ""
+      // 存储要展示的课程表
     };
   },
   onLoad() {
+    this.getSchedulesFromStorage();
   },
   methods: {
-    // 获取并解析存储的数据
+    // 点击某个课程表时，将该课程表设置为要展示的课程表
+    changeScheduleOnShow(schedule) {
+      console.log("原先的课程表：", this.scheduleOnShow);
+      this.scheduleOnShow = schedule;
+      console.log("选中的课程表:", this.scheduleOnShow);
+    },
+    // 获取并解析存储的数据，并找出索引最小的课程表
     getSchedulesFromStorage() {
       try {
         const schedules = common_vendor.index.getStorageSync("schedules");
-        return schedules ? JSON.parse(schedules) : [];
+        if (schedules) {
+          this.schedules = JSON.parse(schedules);
+          if (this.schedules.length > 0) {
+            this.scheduleOnShow = this.schedules.reduce((min, schedule) => {
+              return schedule.index < min.index ? schedule : min;
+            });
+            console.log("选中的课程表:", this.scheduleOnShow);
+          }
+        } else {
+          console.log("未找到课程数据");
+        }
       } catch (e) {
         console.error("Error parsing schedules from storage:", e);
-        return [];
+        this.schedules = [];
       }
     },
     showDrawer(e) {
@@ -40,20 +58,35 @@ const _sfc_main = {
   }
 };
 if (!Array) {
+  const _easycom_uni_list_item2 = common_vendor.resolveComponent("uni-list-item");
+  const _easycom_uni_list2 = common_vendor.resolveComponent("uni-list");
   const _easycom_uni_drawer2 = common_vendor.resolveComponent("uni-drawer");
-  _easycom_uni_drawer2();
+  (_easycom_uni_list_item2 + _easycom_uni_list2 + _easycom_uni_drawer2)();
 }
+const _easycom_uni_list_item = () => "../../uni_modules/uni-list/components/uni-list-item/uni-list-item.js";
+const _easycom_uni_list = () => "../../uni_modules/uni-list/components/uni-list/uni-list.js";
 const _easycom_uni_drawer = () => "../../uni_modules/uni-drawer/components/uni-drawer/uni-drawer.js";
 if (!Math) {
-  _easycom_uni_drawer();
+  (_easycom_uni_list_item + _easycom_uni_list + _easycom_uni_drawer)();
 }
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return {
     a: common_vendor.o(($event) => $options.showDrawer("showLeft")),
-    b: common_vendor.o(($event) => $options.closeDrawer("showLeft")),
-    c: common_vendor.sr("showLeft", "0a28a204-0"),
-    d: common_vendor.o(($event) => $options.change($event, "showLeft")),
-    e: common_vendor.p({
+    b: common_vendor.f($data.schedules, (schedule, index, i0) => {
+      return {
+        a: index,
+        b: common_vendor.o(($event) => $options.changeScheduleOnShow(schedule), index),
+        c: "0a28a204-2-" + i0 + ",0a28a204-1",
+        d: common_vendor.p({
+          title: schedule.name,
+          clickable: true
+        })
+      };
+    }),
+    c: common_vendor.o(($event) => $options.closeDrawer("showLeft")),
+    d: common_vendor.sr("showLeft", "0a28a204-0"),
+    e: common_vendor.o(($event) => $options.change($event, "showLeft")),
+    f: common_vendor.p({
       mode: "left",
       width: 320
     })
